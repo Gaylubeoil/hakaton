65e39cf38b5ce761a493af1056153a3aea4b3de5
gaylubeoil <Floggy7x@gmail.com>
2024-03-16 12:53:18 +0200

    Venci left bar.

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -4,7 +4,7 @@ const LeftBar = () => {
     <div className="leftBar">
       <div className="container">
         <div className="menu">
-          <div className="user">
+          <div className="menuComponent">
             <img
               src="https://images.pexels.com/photos/3766479/pexels-photo-3766479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt=""
@@ -12,9 +12,28 @@ const LeftBar = () => {
             <span>Luben Lubenus</span>
           </div>
 
-          <div className="followers">
+          <div className="menuComponent">
             <img src="" alt="" />
             <span>Followers</span>
+          </div>
+
+          <div className="menuComponent">
+            <img src="" alt="" />
+            <span>Following</span>
+          </div>
+
+          <div className="menuComponent">
+            <img src="" alt=""></img>
+            <span>Events</span>
+          </div>
+
+          <div className="menuComponent">
+            <img src="" alt=""></img>
+            <span>Requests</span>
+          </div>
+          <hr />
+          <div className="filtter">
+            <h3>Filtters</h3>
           </div>
         </div>
       </div>
