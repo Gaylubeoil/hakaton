190d0615bcf4f2624034b80f1fbb288a2ad88439
gaylubeoil <Floggy7x@gmail.com>
2024-03-17 13:30:37 +0200

    Added a profile page.

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,41 @@
-import './profile.scss'
+import "./profile.scss";
+import { userPropTypes } from "../../PropTypesDefinitions.js";
 
-const Profile = () => {
+import { SocialIcon } from "react-social-icons";
+
+const Profile = ({ user }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="profile">
+      <img src={user.image} className="profile" />
+      <div className="container">
+        <div className="username">
+          <span>{user.username}</span>
+        </div>
+        <div className="user-data">
+          <button className="follow-button">Follow</button>
+          <span>Trust Score: {user.trustScore}</span>
+          <span>Email: {user.email}</span>
+          <span>Bio: {user.bio}</span>
+        </div>
+        <div className="icons">
+          <SocialIcon url="https://instagram.com" />
+          <SocialIcon url="https://discord.com" />
+          <SocialIcon url="https://facebook.com" />
+          <SocialIcon url="https://reddit.com" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+Profile.propTypes = {
+  user: userPropTypes,
+};
+
+{
+  /* <SocialIcon url="https://twitter.com" />
+<SocialIcon url="https://instagram.com" />
+<SocialIcon url="https://youtube.com" />
+<SocialIcon url="https://reddit.com" /> */
+}
+export default Profile;
